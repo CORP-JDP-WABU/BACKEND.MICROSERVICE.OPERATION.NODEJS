71cf03f804301bfe0fcaa6801aa8d27e1da3691d
tismart-fernando <tismart.fernando@gmail.com>
2024-02-14 16:33:29 -0500

    feat: ignor quantity to equal qualification

--- src/modules/qualification/services/fn-qualification-ignorant.service.ts
+++ src/modules/qualification/services/fn-qualification-ignorant.service.ts
@@ -35,12 +35,30 @@ export class FnQualificationIgnorantService {
     this.logger.debug(
       `::pendingToQualification::before::${careerCourseTeacherForStudent.pendingToQualification.length}`,
     );
+      
     const hasIgnorantUpdate =
       careerCourseTeacherForStudent.pendingToQualification.find(
         (elemento) =>
           elemento.course.idCourse == idCourse &&
           elemento.teacher.idTeacher == idTeacher,
       );
+    
+    const manyQualification = careerCourseTeacherForStudent.manyQualification;
+    const manyIgnor = careerCourseTeacherForStudent.pendingToQualification.filter(x => x.hasIgnor).length;
+
+    if(manyQualification == (manyIgnor + 1)) {
+      await this.careerCourseTeacherModel.updateMany({ _id: careerCourseTeacherForStudent._id }, {
+        $set: { "pendingToQualification.$[].hasIgnor": false },
+        multi: true
+      });
+      return <response.ResponseGenericDto>{
+        message: 'Processo exitoso',
+        operation: `::${FnQualificationIgnorantService.name}::execute`,
+        data: {
+          isRemoveTeacherToList: false
+        },
+      };
+    }
 
     hasIgnorantUpdate.hasIgnor = true;
 
